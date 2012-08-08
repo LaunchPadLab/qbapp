f978ddacb65cfb0f14b95521a2d005dbbe086c0e
bhennes2 <bhennes2@gmail.com>
2012-08-08 08:52:53 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qbapp::Application.config.secret_token = '73bcbedc86c3875f476144819e0e44ed49345acc3c249961ac53245c20f37e71ab452020741ded4f0518175767c42eb15bb87d0043ba267b10b3098acd7a4f8b'
