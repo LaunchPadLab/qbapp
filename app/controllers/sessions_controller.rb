5a5ec9d334914fa71d8ae6000ade7d556f205db1
bhennes2 <bhennes2@gmail.com>
2012-08-08 16:24:33 -0500

    Added callback url

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,12 +8,19 @@ class SessionsController < ApplicationController
         :authorize_url        => "https://appcenter.intuit.com/Connect/Begin",
         :access_token_path    => "/oauth/v1/get_access_token"
     })
-    @request_token = qb_oauth_consumer.get_request_token
-    puts @request_token.inspect
+    
+    @callback_url = "/oauth/callback"
+
+    @request_token = qb_oauth_consumer.get_request_token(:oauth_callback => @callback_url)
     session[:request_token] = @request_token
-    redirect_to @request_token.authorize_url
+    redirect_to @request_token.authorize_url(:oauth_callback => @callback_url)
     
     #oauth_client = OAuth::AccessToken.new(qb_oauth_consumer, access_token, access_secret)
+  end
+  
+  def callback
+    @access_token = @request_token.get_access_token
+    #@photos = @access_token.get('/photos.xml')
   end
   
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Qbapp::Application.routes.draw do
 
   root :to => 'pages#home'
+  
   get '/dashboard' => 'pages#dashboard', as: :dashboard
+  
   get '/SendOpenIDRequest' => 'sessions#request_id'
   
+  get '/oauth/callback' => 'sessions#callback'
 
 end
