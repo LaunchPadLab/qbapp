322bf1d944863488e34c6edfee5982e7ad74e5af
bhennes2 <bhennes2@gmail.com>
2012-08-08 09:24:44 -0500

    Comment out Oauth

--- config/initializers/qb.rb
+++ config/initializers/qb.rb
@@ -1,11 +1,11 @@
-QB_KEY = ENV['QB_KEY']
-QB_SECRET = ENV['QB_SECRET']
-
-qb_oauth_consumer = OAuth::Consumer.new(QB_KEY, QB_SECRET, {
-    :site                 => "https://oauth.intuit.com",
-    :request_token_path   => "/oauth/v1/get_request_token",
-    :authorize_url        => "https://appcenter.intuit.com/Connect/Begin",
-    :access_token_path    => "/oauth/v1/get_access_token"
-})
-
-oauth_client = OAuth::AccessToken.new(qb_oauth_consumer, access_token, access_secret)
+# QB_KEY = ENV['QB_KEY']
+# QB_SECRET = ENV['QB_SECRET']
+# 
+# qb_oauth_consumer = OAuth::Consumer.new(QB_KEY, QB_SECRET, {
+#     :site                 => "https://oauth.intuit.com",
+#     :request_token_path   => "/oauth/v1/get_request_token",
+#     :authorize_url        => "https://appcenter.intuit.com/Connect/Begin",
+#     :access_token_path    => "/oauth/v1/get_access_token"
+# })
+# 
+# oauth_client = OAuth::AccessToken.new(qb_oauth_consumer, access_token, access_secret)
